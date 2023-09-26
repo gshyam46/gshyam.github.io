11c83ec4ae39b5f5998d7f3b254646cd776948df
Ghanashyam G <gshyam2603@gmail.com>
2023-09-27 03:36:37 +0530

    Initial Commit(Working)

---
+++ src/themes/default.js
@@ -0,0 +1,37 @@
+export default {
+  // Temp fonts
+  fonts: {
+    title: "Space Grotesk, sans-serif",
+    main: "Space Grotesk, sans-serif",
+  },
+  // Colors for layout
+  // colors: {
+  //   primary1: "#854CE6",
+  //   background1: "#222A35",
+  //   button: "#854CE6",
+  //   background2: "#19212C",
+  //   text: "#C8CFD8",
+  //   text1: "#F2F5F7",
+  //   text2: "#626970",
+  //   text3: "#575C66",
+  //   footerBackground: "#00012B",
+  // },
+  colors: {
+    primary1: "#44BFC3", // Primary color
+    background1: "#0A2919", // Dark green background
+    button: "#5DBB63", // Green button
+    background2: "#183D25", // Slightly lighter green background
+    text: "#D0E5D9", // Light text color
+    text1: "#F4FBF8", // Very light text color
+    text2: "#7EA37C", // Darker green text
+    text3: "#6A8E60", // Even darker green text
+    footerBackground: "#001D0A", // Dark footer background
+  },
+  // Breakpoints for responsive design
+  breakpoints: {
+    sm: "screen and (max-width: 640px)",
+    md: "screen and (max-width: 768px)",
+    lg: "screen and (max-width: 1024px)",
+    xl: "screen and (max-width: 1280px)",
+  },
+};
